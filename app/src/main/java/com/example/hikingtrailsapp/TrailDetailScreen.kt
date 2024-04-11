68af22f95eccdbfc2432cf4f76105a950e469e94
Alan Kowańdy <alankowandy25@gmail.com>
2024-04-11 10:30:38 +0200

    Initial implementation of the timer

--- app/src/main/java/com/example/hikingtrailsapp/TrailDetailScreen.kt
+++ app/src/main/java/com/example/hikingtrailsapp/TrailDetailScreen.kt
@@ -2,6 +2,7 @@ package com.example.hikingtrailsapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,16 +12,27 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.delay
+import java.util.concurrent.TimeUnit
 
 @Composable
 fun TrailDetailScreen(trail: Trail){
@@ -47,5 +59,68 @@ fun TrailDetailScreen(trail: Trail){
         Text(text = trail.shortDesc,
             textAlign = TextAlign.Justify,
             style = MaterialTheme.typography.bodyMedium)
+        Timer()
     }
+}
+
+@Composable
+fun Timer(){
+    var time by remember {
+        mutableStateOf(0L)
+    }
+
+    var isRunning by remember {
+        mutableStateOf(false)
+    }
+
+    var startTime by remember {
+        mutableStateOf(0L)
+    }
+
+    val context = LocalContext.current
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = formatTime(timeMi = time),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(6.dp)
+        )
+        Row {
+            Button(onClick = {
+                if (isRunning){
+                    isRunning = false
+                } else {
+                    startTime = System.currentTimeMillis() - time
+                    isRunning = true
+                    keyboardController?.hide()
+                }
+            }) {
+                Text(
+                    text = if (isRunning) "Pause" else "Start", color = Color.White
+                )
+            }
+        }
+    }
+
+    LaunchedEffect(isRunning){
+        while (isRunning){
+            delay(1000)
+            time = System.currentTimeMillis()-startTime
+        }
+    }
+}
+
+@Composable
+fun formatTime(timeMi:Long):String{
+    val hours = TimeUnit.MILLISECONDS.toHours(timeMi)
+    val min = TimeUnit.MILLISECONDS.toMinutes(timeMi)
+    val sec = TimeUnit.MILLISECONDS.toSeconds(timeMi)
+
+    return String.format("%02d:%02d:%02d", hours, min, sec)
 }
